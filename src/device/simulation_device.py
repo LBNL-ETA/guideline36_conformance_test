8818d4b46f807bb3a71e57c85ef936c78bad77f7
David Blum <dhblum@lbl.gov>
2026-02-05 07:17:34 -0800

    Move class map to point map file

--- src/device/simulation_device.py
+++ src/device/simulation_device.py
@@ -110,12 +110,6 @@ class SimulationDevice(BaseDevice):
         # Read point map CSV (skip first 3 header rows, use 'Variable Name' as index)
         df_pointmap = pd.read_csv(filepath_pointmap, header=3, index_col='Variable Name')
         
-        # Get CDL Model Class-Instance Map from device config
-        # This maps full CDL class names to the instance names used in the .mo file
-        if 'cdl_class_instance_map' not in self.device_config:
-            raise ValueError("device_config must include 'cdl_class_instance_map' to map CDL classes to model instance names")
-        class_map = self.device_config['cdl_class_instance_map']
-        
         # Track output and parameter names for FMU initialization
         self.output_names = []
         self.parameter_names = []
@@ -136,10 +130,10 @@ class SimulationDevice(BaseDevice):
             if causality == 'Input':
                 cdl_path = cdl_name
             elif causality == 'Parameter':
-                cdl_path = f"{class_map.get(cdl_block, 'unknown')}.{cdl_name}"
+                cdl_path = f"{cdl_block}.{cdl_name}"
                 self.parameter_names.append(cdl_path)
             elif causality in ['Output', 'State']:
-                cdl_path = f"{class_map.get(cdl_block, 'unknown')}.{cdl_name}"
+                cdl_path = f"{cdl_block}.{cdl_name}"
                 self.output_names.append(cdl_path)
             else:
                 continue
