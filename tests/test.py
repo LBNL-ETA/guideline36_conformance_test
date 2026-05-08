993e25e3437011f1275808a60bdf3835884399a6
Majid alDosari <majid.aldosari@pnnl.gov>
2026-05-08 16:35:53 -0400

    locate result pth

--- tests/test.py
+++ tests/test.py
@@ -7,7 +7,14 @@ from pathlib import Path
 @pytest.mark.parametrize("test_type", [
 'foo_units',
 ])
-def test(name):
-    gtest.start_test(to_csv=True, name=name)        
-    assert(True)
+def test(test_type):
+    name = test_type
+    gtest.start_test(to_csv=True, name=name)
 
+    # conf = Path('conformance_tests') / test_type / 'config' / 'config.yaml'
+    # assert(conf.exists())
+    # from yaml import safe_load
+    # conf = safe_load(open(conf))
+    result = Path('conformance_tests') / name / 'results' / f'run_{name}' / \
+        f'{name}_values.csv'
+    assert(result.exists())
